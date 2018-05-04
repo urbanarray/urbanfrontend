ea40ce8813adbaa8e4b926a5e8452dd8ff2d5313
mmarovich <mmarovich@gmail.com>
2018-05-04 13:17:32 -0500

    injected colors to Roleview page and component cleanup

--- app/containers/RoleView/RoleDisplay/RoleDisplay.js
+++ app/containers/RoleView/RoleDisplay/RoleDisplay.js
@@ -1,41 +1,40 @@
 import React from 'react';
 
 import { Col, Table } from 'react-bootstrap';
+import { styles } from '../../../assets/styles/variables';
 
-const RoleDisplay = (props) => {
-    console.log(props);
-    return (
-        <Col md={12}>
-            <div id="panelDemo8" className="panel panel-primary">
-                <div className="panel-heading">
-                    Role: {props.roleview.roleDetail.role}
-                </div>
-
-                { /* START table-responsive */}
-                <Table id="table-ext-2" responsive striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Project</th>
-                            <th>Date/Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                {props.roleview.roleDetail.project}
-                            </td>
-                            <td>
-                                {props.roleview.roleDetail.date + ' ' + props.roleview.roleDetail.startTime + ' to ' + this.props.roleview.roleDetail.endTime}
-                            </td>
-                        </tr>
-                    </tbody>
-                </Table>
-                { /* END table-responsive */}
-
-                {/* <div className="panel-footer">Panel Footer</div> */}
+const RoleDisplay = (props) => (
+    <Col md={12}>
+        <div id="panelDemo8" className="panel panel-primary">
+            <div className="panel-heading" style={styles.primaryDark}>
+                Role: {props.roleview.roleDetail.role}
             </div>
-        </Col>
-    )
-}
+
+            { /* START table-responsive */}
+            <Table id="table-ext-2" responsive striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Project</th>
+                        <th>Date/Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            {props.roleview.roleDetail.project}
+                        </td>
+                        <td>
+                            {`${props.roleview.roleDetail.date} 
+                                ${props.roleview.roleDetail.startTime} to ${props.roleview.roleDetail.endTime}`}
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+            { /* END table-responsive */}
+
+            {/* <div className="panel-footer">Panel Footer</div> */}
+        </div>
+    </Col>
+)
 
 export default RoleDisplay;
