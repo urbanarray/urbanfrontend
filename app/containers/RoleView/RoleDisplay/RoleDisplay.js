ef501848a1f6d71e17c6e741e16e4b23e91b70c7
mmarovich <mmarovich@gmail.com>
2018-07-01 21:16:21 -0500

    added RoleDisplay responsiveness

--- app/containers/RoleView/RoleDisplay/RoleDisplay.js
+++ app/containers/RoleView/RoleDisplay/RoleDisplay.js
@@ -1,43 +1,85 @@
 import React from 'react';
 
-import { Col, Table } from 'react-bootstrap';
+import { Col, Table, Panel } from 'react-bootstrap';
 import { styles } from '../../../assets/styles/variables';
 
 const RoleDisplay = (props) => {
-    console.log(props.windowWidth)
-    return (
-        <Col md={12}>
-            <div id="panelDemo8" className="panel panel-primary">
-                <div className="panel-heading" style={styles.primaryDark}>
-                    Role: {props.roleview.roleDetail.role}
-                </div>
 
-                { /* START table-responsive */}
-                <Table id="table-ext-2" responsive striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Project</th>
-                            <th>Date/Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                {props.roleview.roleDetail.project}
-                            </td>
-                            <td>
-                                {`${props.roleview.roleDetail.date} 
+    const renderHeader = () => {
+        if (props.windowWidth < 600) {
+            return null;
+        } else {
+            return (
+                <thead>
+                    <tr>
+                        <th>Project</th>
+                        <th>Date/Time</th>
+                    </tr>
+                </thead>
+            )
+        }
+    }
+
+    const renderTable = () => {
+        if (props.windowWidth < 600) {
+            return (
+                <Panel bsStyle="primary">
+                    <Panel.Heading style={{ textAlign: 'center', backgroundColor: 'white', color: 'black' }}>
+                        <Panel.Title componentClass="h3">{props.roleview.roleDetail.role}</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body style={{ textAlign: 'center' }}>
+                        Project: {props.roleview.roleDetail.project}<br />
+                        Date: {props.roleview.roleDetail.date}<br />
+                        Time: {`${props.roleview.roleDetail.startTime} - ${props.roleview.roleDetail.endTime}`}<br />
+                    </Panel.Body>
+                </Panel>
+            )
+        } else {
+            return (
+                <tbody>
+                    <tr>
+                        <td>
+                            {props.roleview.roleDetail.project}
+                        </td>
+                        <td>
+                            {`${props.roleview.roleDetail.date} 
                                 ${props.roleview.roleDetail.startTime} to ${props.roleview.roleDetail.endTime}`}
-                            </td>
-                        </tr>
-                    </tbody>
-                </Table>
-                { /* END table-responsive */}
+                        </td>
+                    </tr>
+                </tbody>
+            )
+        }
+    }
 
-                {/* <div className="panel-footer">Panel Footer</div> */}
-            </div>
-        </Col>
-    )
+    if (props.windowWidth < 600) {
+        return (
+            <Col md={12}>
+                <div id="panelDemo8" className="panel panel-primary">
+                    <div className="panel-heading" style={styles.primaryDark}>
+                        Role: {props.roleview.roleDetail.role}
+                    </div>
+    
+                    {renderHeader()}
+                    {renderTable()}
+                </div>
+            </Col>
+        )
+    } else {
+        return (
+            <Col md={12}>
+                <div id="panelDemo8" className="panel panel-primary">
+                    <div className="panel-heading" style={styles.primaryDark}>
+                        Role: {props.roleview.roleDetail.role}
+                    </div>
+    
+                    <Table id="table-ext-2" responsive striped bordered hover>
+                        {renderHeader()}
+                        {renderTable()}
+                    </Table>
+                </div>
+            </Col>
+        )
+    }
 }
 
 export default RoleDisplay;
