0e289019daed90f901c5e48d6cd90006d3cf26a3
Tyler Earls <tyler.a.earls@gmail.com>
2018-07-22 10:56:48 -0500

    set up file structure for Onboarding folder for 3 step views with file templates

---
+++ app/containers/Onboarding/Views/GetInvolved/GetInvolved.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import { Col } from 'react-bootstrap';
+import { styles, headings } from 'assets/styles/variables';
+
+
+export default class GetInvolved extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <Col lg={6}>
+        { /* START panel */}
+        <div id="panelDemo8" className="panel panel-default panel-demo">
+          <div className="panel-heading" style={styles.primaryDark}>
+            <h4 style={headings.tableHeading}>Location</h4>
+          </div>
+          <div className="panel-body text-center">
+            <p>
+              {this.props.location.name.toUpperCase()}
+            </p>
+          </div>
+        </div>
+        { /* END panel */}
+      </Col>
+    )
+  }
+}
